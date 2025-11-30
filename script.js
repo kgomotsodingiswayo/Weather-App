9592858f1a310e1dd5715e125cce7d8b10d7586e
kgomotsodingiswayo <motsopetlele95@gmail.com>
2025-11-30 21:00:58 +0200

    Added search functionality using API

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    let cityElement = document.querySelector("#city");
+
+ cityElement.innerHTML = response.data.city;
+ temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+}
+function searchCity(city) { 
+let apiKey = "tf6b10721ee3fc0cdao3e7c56700ab34";
+let apiUrl =
+  `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+ axios.get(apiUrl).then(refreshWeather) ;
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+ 
+    searchCity(searchInput.value);
 }
 
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Cape Town");
