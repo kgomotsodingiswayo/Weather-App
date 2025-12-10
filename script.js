b83494f39b462c5eb2a153853237e1bc4f657428
kgomotsodingiswayo <motsopetlele95@gmail.com>
2025-12-10 16:06:04 +0200

    Injected a forecast HTML from JS

--- script.js
+++ script.js
@@ -1,44 +1,65 @@
 function refreshWeather(response) {
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
-    let cityElement = document.querySelector("#city");
-    let descriptionElement = document.querySelector("#description");
-    let humidityElement = document.querySelector("#humidity");
-    let windSpeedElement = document.querySelector("#wind-speed");
-    let timeElement = document.querySelector("#time");
-     let date = new Date(response.data.time * 1000);
-     let iconElement= document.querySelector("#icon");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
-     iconElement.innerHTML =
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-temp-icon"/>`;
 
-       `<img src="${response.data.condition.icon_url}" class="weather-app-temp-icon"/>`;
-
- cityElement.innerHTML = response.data.city;
-    timeElement.innerHTML = date.toLocaleString("en-US", {
-        weekday: "long",
-        hour: "2-digit",
-        minute: "2-digit"
-    });
- descriptionElement.innerHTML = response.data.condition.description;
-    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-    windSpeedElement.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
- temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = date.toLocaleString("en-US", {
+    weekday: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 }
-function searchCity(city) { 
-let apiKey = "tf6b10721ee3fc0cdao3e7c56700ab34";
-let apiUrl =
-  `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
- axios.get(apiUrl).then(refreshWeather) ;
+function searchCity(city) {
+  let apiKey = "tf6b10721ee3fc0cdao3e7c56700ab34";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
- 
-    searchCity(searchInput.value);
+
+  searchCity(searchInput.value);
 }
 
+function displayForecast() {
+   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+   <div class="weather-forecast-days">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">⛅</div> 
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+                <strong>18°</strong>
+          </div>
+          <div class="weather-forecast-temperature">
+            7°</div>
+       </div>
+      </div>
+      `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Cape Town");
+displayForecast();
